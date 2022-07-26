ffe8c85b8fa83308af280bbb0cf1fa47c61a51fd
Kate Kasatkina <kasatkina_katia@mail.ru>
2022-07-26 14:05:01 +0300

    task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int A = 0;
+int B = 0;
+
+Console.Write("Введите трехзначное число: ");
+A = int.Parse(Console.ReadLine());
+
+B = A / 10 % 10;
+
+System.Console.WriteLine($"Вторая цифра числа {A} - это {B} ");
+
