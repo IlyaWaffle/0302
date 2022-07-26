47d97bd0797b3db610be702ad1bd902d1648bd62
Kate Kasatkina <kasatkina_katia@mail.ru>
2022-07-26 15:50:02 +0300

    task 15

---
+++ Task15_weekend/Program.cs
@@ -0,0 +1,11 @@
+﻿int day = 0;
+
+
+Console.Write("Введите число от 1 до 7: ");
+day = int.Parse(Console.ReadLine());
+
+if (day < 6)
+    Console.Write ("Это рабочий день");
+
+else 
+ Console.Write ("Это выходной");
