a299a6b34fb97d4b7060b9845f3f8e9805d73158
Kate Kasatkina <kasatkina_katia@mail.ru>
2022-07-27 17:41:32 +0300

    task 13 error

---
+++ Task13_third/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA = 0;
+int numberB = 0;
+
+Console.Write("Введите число: ");
+numberA = int.Parse(Console.ReadLine());
+
+
+if (numberA > 100);
+ {
+    while (numberA > 1000)
+{
+    numberA /= 10;
+}
+    
+    numberB = numberA % 10;
+    Console.Write($"Третья цифра числа {numberA} - это {numberB} ");
+ }
+else 
+ Console.Write ($"В числе {numberA} нет третьей цифры"); 
+ 
+
